b7a230e1baa8029c5ab028b01fd6bc3e62155868
leersja <jan.leers@datasense.be>
2025-09-15 17:08:43 +0200

    stuff

--- models/staging/stg_jaffle_shop__customers.sql
+++ models/staging/stg_jaffle_shop__customers.sql
@@ -1,6 +1,5 @@
-select
+select 
     id as customer_id,
     first_name,
-    last_name,
-    'test' as tst
-from raw.jaffle_shop.customers
+    last_name
+from {{ source('jaffle_shop', 'customers') }}
--- models/staging/stg_jaffle_shop__orders.sql
+++ models/staging/stg_jaffle_shop__orders.sql
@@ -2,7 +2,5 @@ select
     id as order_id,
     user_id as customer_id,
     order_date,
-    status,
-    _etl_loaded_at
-
-from raw.jaffle_shop.orders
+    status
+from {{ source('jaffle_shop', 'orders') }}
